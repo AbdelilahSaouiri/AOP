58af0c3d44b5f20ec4796e8cd5bdc6e77525a1cc
abdelilah <abdelilah.saouir@etu.uae.ac.ma>
2025-06-21 20:18:09 +0100

    add spring aop

---
+++ src/main/java/net/ensah/service/IUserService.java
@@ -0,0 +1,7 @@
+package net.ensah.service;
+
+public interface IUserService {
+
+    void add(String name);
+    void delete(String name);
+}
---
+++ src/main/java/net/ensah/service/IUserServiceImpl.java
@@ -0,0 +1,22 @@
+package net.ensah.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class IUserServiceImpl implements IUserService {
+
+    List<String> users = new ArrayList<>();
+
+    @Override
+    public void add(String name) {
+     users.add(name);
+    }
+
+    @Override
+    public void delete(String name) {
+     users.remove(name);
+    }
+}
