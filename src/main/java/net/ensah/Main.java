7cd673eac6bd86925d7c7c0ed683ad9ebf7897d4
abdelilah <abdelilah.saouir@etu.uae.ac.ma>
2025-06-22 23:08:49 +0100

    update

--- src/main/java/net/ensah/Main.java
+++ src/main/java/net/ensah/Main.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.ComponentScan;
 
 
-@ComponentScan(gi})
+@ComponentScan(value = {"net.ensah.config","net.ensah.service","net.ensah.springAspect"})
 public class Main {
     public static void main(String[] args) throws InterruptedException {
 
